abbbb5c2ce6614d185c1436d7c3c0b03ca448ae6
mkarthauser <mikek@brightstorm.co.uk>
2020-10-20 21:39:26 +0100

    displays category tree from top level category

--- project/resources/views/layouts/front/category-nav.blade.php
+++ project/resources/views/layouts/front/category-nav.blade.php
@@ -1,11 +1,18 @@
-<ul class="list-unstyled list-inline nav navbar-nav">
-    @foreach($categories as $category)
-        <li>
-            @if($category->children()->count() > 0)
-                @include('layouts.front.category-sub', ['subs' => $category->children])
-            @else
-                <a @if(request()->segment(2) == $category->slug) class="active" @endif href="{{route('front.category.slug', $category->slug)}}">{{$category->name}} </a>
+
+@foreach($rootCategories as $category)
+    @if($loop->first)
+        <ul class="list-unstyled list-inline nav navbar-nav">
             @endif
-        </li>
-    @endforeach
-</ul>
+            <li>
+                @if($category->children()->count() > 0)
+                      @include('layouts.front.category-sub', ['subs' => $category->children])
+                @else
+                    <a @if(request()->segment(2) === $category->slug) class="active"
+                       @endif href="{{route('front.category.slug', $category->slug)}}">{{$category->name}} </a>
+                @endif
+
+            </li>
+            @if($loop->last)
+        </ul>
+    @endif
+@endforeach
