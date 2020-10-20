ddc3a0c87ee115b14908a06cf002815c46f071ee
mkarthauser <mikek@brightstorm.co.uk>
2020-10-20 21:41:23 +0100

    adds category top level parent and its children

--- project/resources/views/front/categories/sidebar-category.blade.php
+++ project/resources/views/front/categories/sidebar-category.blade.php
@@ -1,9 +1,25 @@
-<ul class="nav sidebar-menu">
-    @foreach($categories as $category)
-        @if($category->children()->count() > 0)
-            <li>@include('layouts.front.category-sidebar-sub', ['subs' => $category->children])</li>
-        @else
-            <li @if(request()->segment(2) == $category->slug) class="active" @endif><a href="{{ route('front.category.slug', $category->slug) }}">{{ $category->name }}</a></li>
+@isset($parentCategory)
+    <h2><a @if(request()->segment(2) === $parentCategory->slug) class="active" @endif
+        href="{{ route('front.category.slug', $parentCategory->slug) }}">{{ $parentCategory->name }}</a>
+    </h2>
+@endisset
+
+@isset($childCategories)
+    @foreach($childCategories as $category)
+        @if($loop->first)
+            <ul class="nav sidebar-menu">
+                @endif
+
+                @if($category->children()->count() > 0)
+                    <li>@include('layouts.front.category-sidebar-sub', ['subs' => $category->children])</li>
+                @else
+                    <li @if(request()->segment(2) === $category->slug) class="active" @endif><a
+                                href="{{ route('front.category.slug', $category->slug) }}">{{ $category->name }}</a>
+                    </li>
+                @endif
+
+                @if($loop->last)
+            </ul>
         @endif
     @endforeach
-</ul>
+@endisset
