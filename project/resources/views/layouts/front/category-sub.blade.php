2aafd21a43d5398cc4eff8d771f87f0ffd20c214
mkarthauser <mikek@brightstorm.co.uk>
2020-10-20 21:37:37 +0100

    splits top navigation so main links are clickable and to get dropdown you press the arrow

--- project/resources/views/layouts/front/category-sub.blade.php
+++ project/resources/views/layouts/front/category-sub.blade.php
@@ -1,6 +1,19 @@
-<div class="dropdown">
-    <a @if(request()->segment(2) == $category->slug) class="active" @endif href="{{route('front.category.slug', $category->slug)}}" class="dropdown-toggle" id="{{$category->slug}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">{{$category->name}} <span class="caret"></span></a>
-    <ul class="dropdown-menu" aria-labelledby="{{$category->slug}}">
+<div class="dropdown" id="main-menu">
+
+    <a @if(request()->segment(2) === $category->slug)
+       class="active"
+       @endif
+       href="{{route('front.category.slug', $category->slug)}}">{{$category->name}}</a>
+    <a
+       class="dropdown-toggle"
+       id="{{$category->slug}}"
+       data-toggle="dropdown"
+       aria-haspopup="true"
+       aria-expanded="true"><span class="caret"></span></a>
+
+    <ul class="dropdown-menu"
+        aria-labelledby="{{$category->slug}}"
+    >
         @foreach($subs as $sub)
             <li><a href="{{route('front.category.slug', $sub->slug)}}">{{$sub->name}}</a></li>
         @endforeach
